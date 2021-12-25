aef4c87b9493bf0bad6d647bb7b278ee71b8cec9
sahil negi <sahilnegi658@gmail.com>
2021-12-25 10:57:51 +0530

    Updates

---
+++ precache-manifest.4f9eac0bbf12469bdf4064b92e92eee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387ab4b95b6490ffbacb613815e6933b",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "a89a9085007aef288501",
+    "url": "/photo-gallery/static/css/main.9f71fa73.chunk.css"
+  },
+  {
+    "revision": "08d5454df8609ab6175c",
+    "url": "/photo-gallery/static/js/2.818eebb3.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/photo-gallery/static/js/2.818eebb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89a9085007aef288501",
+    "url": "/photo-gallery/static/js/main.56937512.chunk.js"
+  },
+  {
+    "revision": "1c1d0196c35bbd1c6f97",
+    "url": "/photo-gallery/static/js/runtime-main.f4325aa2.js"
+  }
+]);
